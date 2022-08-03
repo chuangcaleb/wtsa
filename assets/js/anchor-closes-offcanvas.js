2080d30bf1cd91951381e53f1427141abde219ad
chuangcaleb <balechuang@gmail.com>
2022-08-03 21:11:03 +0100

    resp: Condition pop offcanvas only if mobile

--- assets/js/anchor-closes-offcanvas.js
+++ assets/js/anchor-closes-offcanvas.js
@@ -5,15 +5,18 @@
 // 	}
 // });
 document.addEventListener("DOMContentLoaded", function () {
-	var myOffcanvas = document.getElementById("offcanvasNavbar");
-	var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+	// If above xl-breakpoint — if changing screen width, must reload page, which is a negligible problem
+	if (document.documentElement.clientWidth < 1200) {
+		var myOffcanvas = document.getElementById("offcanvasNavbar");
+		var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
 
-	const toc_anchors = document.querySelectorAll("#toc a");
-	for (let i = 0; i < toc_anchors.length; i++) {
-		toc_anchors[i].addEventListener("click", function (e) {
-			bsOffcanvas.toggle();
-			e.stopPropagation();
-			// e.preventDefault();
-		});
+		const toc_anchors = document.querySelectorAll("#toc a");
+		for (let i = 0; i < toc_anchors.length; i++) {
+			toc_anchors[i].addEventListener("click", function (e) {
+				bsOffcanvas.toggle();
+				e.stopPropagation();
+				// e.preventDefault();
+			});
+		}
 	}
 });
