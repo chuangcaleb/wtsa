8007a6dd24fb77bbf8c5058937ecac45a3085c94
chuangcaleb <balechuang@gmail.com>
2022-11-01 19:31:06 +0800

    Cleanup all the javascript

--- assets/js/persistent-offcanvas.js
+++ assets/js/persistent-offcanvas.js
@@ -1,35 +1,37 @@
-var offcanvasButton = document.getElementById("offcanvasButton");
+var offcanvasToggleButton = document.getElementById("offcanvasToggleButton");
 var offcanvasCloseButton = document.getElementById("offcanvasCloseButton");
 var sidebarBody = document.getElementById("sidebarBody");
 var offcanvas = document.getElementById("offcanvasNavbar");
 
 window.addEventListener("DOMContentLoaded", function () {
-	initOffcanvas();
-	offcanvasButton.addEventListener("click", function () {
-		resetOffcanvas();
-	});
-	offcanvasCloseButton.addEventListener("click", function () {
-		resetOffcanvas();
-	});
+  initOffcanvas();
+  offcanvasToggleButton.addEventListener("click", function () {
+    resetOffcanvas();
+  });
+  offcanvasCloseButton.addEventListener("click", function () {
+    resetOffcanvas();
+  });
 });
+
 function initOffcanvas() {
-	var offcanvasShown =
-		localStorage.getItem("offcanvasNavbar") !== null &&
-		localStorage.getItem("offcanvasNavbar") === "shown";
-	offcanvasShown
-		? offcanvas.classList.add("show")
-		: offcanvas.classList.remove("show");
-	setTimeout(function () {
-		offcanvas.style.transition = "transform 0.3s ease-in-out";
-		sidebarBody.style.transition = "transform 0.3s ease-in-out";
-	}, 10);
+  var offcanvasShown =
+    // localStorage.getItem("offcanvasNavbar") !== null &&
+    localStorage.getItem("offcanvasNavbar") === "shown";
+  offcanvasShown
+    ? offcanvas.classList.add("show")
+    : offcanvas.classList.remove("show");
+  setTimeout(function () {
+    offcanvas.style.transition = "transform 0.3s ease-in-out";
+    sidebarBody.style.transition = "transform 0.3s ease-in-out";
+  }, 1);
 }
+
 function resetOffcanvas() {
-	if (offcanvas.classList.contains("show")) {
-		offcanvas.classList.remove("show");
-		localStorage.removeItem("offcanvasNavbar");
-	} else {
-		offcanvas.classList.add("show");
-		localStorage.setItem("offcanvasNavbar", "shown");
-	}
+  if (offcanvas.classList.contains("show")) {
+    offcanvas.classList.remove("show");
+    localStorage.removeItem("offcanvasNavbar");
+  } else {
+    offcanvas.classList.add("show");
+    localStorage.setItem("offcanvasNavbar", "shown");
+  }
 }
