d1cfd50b87de5c4080f58970d5bdeeeb84fcf6c0
chuangcaleb <balechuang@gmail.com>
2022-11-01 19:32:13 +0800

    Cleanup javascript

--- assets/js/anchor-closes-offcanvas.js
+++ assets/js/anchor-closes-offcanvas.js
@@ -1,23 +1,17 @@
-// window.addEventListener("load", function () {
-// 	const toc_anchors = document.querySelectorAll("#toc a");
-// 	for (let i = 0; i < toc_anchors.length; i++) {
-// 		toc_anchors[i].setAttribute("data-bs-dismiss", "offcanvas");
-// 	}
-// });
 document.addEventListener("DOMContentLoaded", function () {
-	// If above xl-breakpoint — if changing screen width, must reload page, which is a negligible problem
-	if (document.documentElement.clientWidth < 1200) {
-		var myOffcanvas = document.getElementById("offcanvasNavbar");
-		var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+  // If above xl-breakpoint — if changing screen width, must reload page, which is a negligible problem
+  if (document.documentElement.clientWidth < 1200) {
+    var myOffcanvas = document.getElementById("offcanvasNavbar");
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
 
-		const toc_anchors = document.querySelectorAll("#offcanvasNavbar a");
-		for (let i = 0; i < toc_anchors.length; i++) {
-			toc_anchors[i].addEventListener("click", function (e) {
-				bsOffcanvas.toggle();
-				e.stopPropagation();
-				localStorage.removeItem("offcanvasNavbar");
-				// e.preventDefault();
-			});
-		}
-	}
+    const toc_anchors = document.querySelectorAll("#offcanvasNavbar a");
+
+    toc_anchors.forEach((a) => {
+      a.addEventListener("click", function (e) {
+        bsOffcanvas.toggle();
+        e.stopPropagation();
+        localStorage.removeItem("offcanvasNavbar");
+      });
+    });
+  }
 });
